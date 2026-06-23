b7de05e89dad0285d7080114540750a8d8526f47
Vikesh09 <gvk26601@gmail.com>
2026-06-23 18:44:20 +0530

    add

---
+++ A_My_First_Sorting_Problem.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+#define ll long long
+#define endl "\n"
+#define vi vector<int>
+#define vl vector<long long>
+
+using namespace std;
+
+void solve() {
+  int x,y; cin>>x>>y;
+  cout<<min(x,y)<<" "<<max(x,y)<<endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+}
