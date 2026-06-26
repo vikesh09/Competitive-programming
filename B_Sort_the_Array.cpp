d3f8fd0822afbc478019cf080e25759f1471307c
Vikesh09 <gvk26601@gmail.com>
2026-06-26 17:50:57 +0530

    u

--- B_Sort_the_Array.cpp
+++ B_Sort_the_Array.cpp
@@ -14,9 +14,32 @@ using namespace std;
 
 void solve() {
  int n; cin>>n;
- vi a(n);
+ vi a(n),b;
  for(int i=0;i<n;i++){
     cin>>a[i];
+ }
+ b=a;
+ sort(b.begin(),b.end());
+ int l=0;
+ while(l<n && a[l]==b[l]){
+    l++;
+ }
+ if(l==n){
+    cout<<"yes"<<endl;
+    cout<<"1 1"<<endl;
+    return;
+ }
+ int r=n-1;
+ while(r>=0 && a[r]==b[r]){
+    r--;
+ }
+ reverse(a.begin()+l,a.begin()+r+1);
+ if(a==b){
+    cout<<"yes"<<endl;
+    cout<<l+1<<" "<<r+1<<endl;
+ }
+ else{
+    cout<<"no"<<endl;
  }
 
   
