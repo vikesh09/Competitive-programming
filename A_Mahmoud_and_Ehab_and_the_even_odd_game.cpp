09579195e6852b23e0cf209d00a431511c388541
Vikesh09 <gvk26601@gmail.com>
2026-06-28 07:55:37 +0530

    u

---
+++ A_Mahmoud_and_Ehab_and_the_even_odd_game.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+#include <unordered_map>
+
+#define ll long long
+#define endl "\n"
+#define vi vector<int>
+#define vl vector<long long>
+
+using namespace std;
+
+void solve() {
+ ll n; cin>>n;
+ if(n==1){
+    cout<<"Ehab"<<endl;
+ }
+ else if(n%2==0){
+    cout<<"Mahmoud"<<endl;
+ }
+ else{
+    cout<<"Ehab"<<endl;
+ }
+ 
+ 
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+}
