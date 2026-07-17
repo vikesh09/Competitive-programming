39e52931503eb40f3e209dc6926b44de50f33be3
Vikesh09 <gvk26601@gmail.com>
2026-07-17 23:26:25 +0530

    add

---
+++ A_Case_of_the_Zeros_and_Ones.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+#include <numeric>
+#include <unordered_map>
+#include <queue>
+
+#define ll long long
+#define endl "\n"
+#define vi vector<int>
+#define vl vector<long long>
+
+using namespace std;
+
+void solve(){
+    int n; cin>>n;
+    string s; cin>>s;
+    stack<char>st;
+    for(char ch:s){
+        if(!st.empty() && st.top()!=ch){
+            st.pop();
+        }
+        else{
+            st.push(ch);
+
+        }
+    }
+    cout<<st.size()<<endl;
+
+   
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+}
