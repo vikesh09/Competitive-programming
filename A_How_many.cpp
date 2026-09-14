6031cf7d06440ce5f8dedc7430d236a759b6f353
Vikesh09 <gvk26601@gmail.com>
2026-09-14 21:57:00 +0530

    icpc 2025-problem_A

---
+++ A_How_many.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+#include<numeric>
+#include<unordered_map>
+
+#define ll long long
+#define endl "\n"
+#define vi vector<int>
+#define vl vector<long long>
+
+using namespace std;
+
+void solve(){
+    int n; cin>>n;
+    cout<<3*n<<endl;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+    solve();
+}
